43fb4170ffa556b8822f8d12dc762fdbc4475688
Eddy Alvarez <eddyalvarezvillavicencio@gmail.com>
2024-09-27 19:45:12 -0500

    semana 08: dia 03 - asynch - pokemon

--- SEMANA 8/dia 02-02-js-pokemons/main.js
+++ SEMANA 8/dia 02-02-js-pokemons/main.js
@@ -12,9 +12,22 @@ const fetchPokemons = async (page = 1) => {
 
   const data = await response.json() // Convierte la respuesta en formato JSON
 
-  // console.log(data)
+  const dataResults = data.results.map(pokemon => {
+    // "url": "https://pokeapi.co/api/v2/pokemon/1/"
+    const id = pokemon.url.split('/').at(6)
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+  
+    return {
+      ...pokemon,
+      id,
+      image
+    }
+  })
 
-  return data
+  return {
+    ...data,
+    results: dataResults
+  }
 }
 
 const renderPokemons = (pokemons = []) => {
@@ -22,9 +35,12 @@ const renderPokemons = (pokemons = []) => {
 
   let elements = ''
 
+  // TODO: Agregar una imagen a cada pokemon
+
   pokemons.forEach(pokemon => {
     elements += `<article class="pokemon-item">
-      ${pokemon.name}
+      <h2>#${pokemon.id} ${pokemon.name}</h2>
+      <img src="${pokemon.image}" width="80" height="80" />
     </article>`
   })
 
