2687ce30aa7a23d29537e3372a11d88ec7ff1f57
Eddy Alvarez <eddyalvarezvillavicencio@gmail.com>
2024-10-03 23:03:26 -0500

    semana 09: dia 02 - react - todo

--- SEMANA 9/dia02-03-react-todo-app/src/App.jsx
+++ SEMANA 9/dia02-03-react-todo-app/src/App.jsx
@@ -51,6 +51,28 @@ const App = () => {
     setTodos(updatedTodos)
   }
 
+  const handleCompleted = (event) => {
+    const { id } = event.target.dataset
+    const isChecked = event.target.checked
+
+    console.log(id, isChecked)
+
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: isChecked
+        }
+      }
+
+      return todo // Deja el objeto sin modificaciones
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  const completedTodos = todos.filter(todo => todo.completed).length
+
   return (
     <main
       className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-400 rounded-lg shadow-lg p-4"
@@ -70,9 +92,12 @@ const App = () => {
 
       {/* {input} */}
 
+      {/* DONE: RETO1 - Añadir una estadística de cuantas tareas estan completadas y el total de tareas */}
+      {/* TODO: RETO2 - Completar la funcionalidad del botón Limpiar completadas */}
+
       <section className="flex justify-between items-center">
         <span className="font-bold">
-          2 de 3
+          {completedTodos} de {todos.length}
         </span>
         <button
           className="bg-blue-500 text-white rounded-lg px-2 py-1 hover:bg-blue-700 duration-300"
@@ -89,9 +114,14 @@ const App = () => {
                 <input
                   className="mr-2"
                   type="checkbox"
+                  data-id={todo.id}
+                  onChange={handleCompleted}
+                  checked={todo.completed}
                 />
                 <div className="w-full flex justify-between items-center">
-                  <span className="font-medium">
+                  <span
+                    className={`font-medium ${todo.completed ? 'line-through' : '' }`}
+                  >
                     {todo.title}
                   </span>
                   <button
